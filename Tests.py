f5b412e6f6f731c2e2a5ba3112c45e9d83e3bf7e
prokoshin03-blip <prokoshin03@gmail.com>
2026-06-13 18:00:05 +0300

    Create Tests.py

---
+++ Tests.py
@@ -0,0 +1,13 @@
+from solution import max_dragon_power
+
+assert max_dragon_power(1) == 1
+assert max_dragon_power(2) == 2
+assert max_dragon_power(3) == 3
+assert max_dragon_power(4) == 4
+assert max_dragon_power(5) == 6
+assert max_dragon_power(6) == 9
+assert max_dragon_power(7) == 12
+assert max_dragon_power(10) == 36
+assert max_dragon_power(99) == 3 ** 33
+
+print("Все тесты пройдены!")
